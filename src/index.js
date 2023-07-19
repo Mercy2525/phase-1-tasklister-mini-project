fb5bf263eadef599b3be2f0d3cc57a1df61b46d1
Mercy2525 <mercywmuriithi.mm@gmail.com>
2023-07-19 16:04:16 +0300

    ToDo displays

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form= document.querySelector("#create-task-form").addEventListener("submit",function(e){
+    e.preventDefault();
+    
+//get elements
+let input= document.getElementById("new-task-description")
+let list= document.createElement('li')
+list.textContent= `${input.value}`
+let output= document.getElementById("tasks")
+output.appendChild(list)
+  
+  })
+ 
+})
+
+
+
+    
+  
+
+
+
+
+
+
+
+
+
+
+
